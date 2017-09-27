54b91b512a64ea44d161dc31bb415f9abf87ec26
Lewis Long <lewislong@lewislong-macbookpro.roam.corp.google.com>
2017-09-26 17:37:10 -0700

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var purifycss = require('gulp-purifycss');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('sass', function(){
+  return gulp.src('./scss/**/*.scss')
+  .pipe(sass().on('error', sass.logError))
+  .pipe(concat('style.css'))
+  .pipe(purifycss(['./public/app/**/*.js', './views/**/*.jade']))
+  .pipe(cleanCSS({compatibility: 'ie9'}))
+  .pipe(gulp.dest('./public/stylesheets'))
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass','watch']);
